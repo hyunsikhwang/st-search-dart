d2fcfe386d2d5faee6fc7a1d3e116fda8c1de2ee
Hyunsik Hwang <gomting@naver.com>
2026-01-17 20:55:11 +0900

    fixzzz

--- app.py
+++ app.py
@@ -434,8 +434,8 @@ st.markdown("""
         padding: 0.5rem 1rem !important;
     }
     .stMainBlockContainer {
-        padding-top: 0.5rem !important;
-        padding-bottom: 0.5rem !important;
+        padding-top: 1rem !important;
+        padding-bottom: 1rem !important;
     }
     .stTextInput input {
         height: 40px !important;
